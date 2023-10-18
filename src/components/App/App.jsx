012d12ba66927ca40daba0fd380b6e10978d8194
ViktoriiaYakymovych <118008244+ViktoriiaYakymovych@users.noreply.github.com>
2023-10-18 16:50:19 +0300

    a

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+export class App extends Component {
+  state = {
+
+  }
+
+  render() {
+    return (
+      <div>
+        Hello
+      </div>
+    )
+  }
+}
