e7760644a49a398c75cb9d50614d26ae329f7288
ViktoriiaYakymovych <118008244+ViktoriiaYakymovych@users.noreply.github.com>
2023-10-22 11:43:48 +0300

    a

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,17 +7,30 @@ import { ContactList } from 'components/ContactList/ContactList';
 import { Filter } from 'components/Filter/Filter';
 import { Layout } from 'components/Layout';
 
-
+const localStorageKey = 'contacts';
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const savedContacts = localStorage.getItem(localStorageKey);
+    if (savedContacts !== null) {
+      this.setState({
+        contacts: JSON.parse(savedContacts),
+      })
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const {contacts: prevContacts} = prevState;
+    const {contacts: nextContacts} = this.state;
+
+    if(prevContacts !== nextContacts) {
+      localStorage.setItem(localStorageKey, JSON.stringify(nextContacts))
+    }
+  }
 
   addNewContact = newContact => {
     if (this.state.contacts.find(contact => contact.name === newContact.name)) {
